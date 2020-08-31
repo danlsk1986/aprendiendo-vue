09e002c54cecd69cb6779b635d14f836f24302bf
danlsk1986 <danlsk1986@gmail.com>
2020-08-31 16:25:14 -0300

    Actualizando el proyecto

--- aprendiendo-vue/src/components/Peliculas.vue
+++ aprendiendo-vue/src/components/Peliculas.vue
@@ -10,7 +10,10 @@
                         </div>
     
                         <h2>{{pelicula.title}}</h2>
-                        <span class="date">
+                        <span class="date" :class="{
+                            yeargreen: pelicula.year >= 2015,
+                            yearred: pelicula.year < 2015
+                        }">
                             {{pelicula.year}}
                         </span>
                         <a href="#">Leer más</a>
@@ -30,17 +33,22 @@ export default {
     data(){
         return{
             peliculas: [
-                {title: 'Batman Inicia',
-                 year: 2005,
-                 img: 'https://www.ecured.cu/images/thumb/6/63/Batman-inicia.jpg/300px-Batman-inicia.jpg'
+                {   title: 'Batman Inicia',
+                    year: 2005,
+                    img: 'https://www.ecured.cu/images/thumb/6/63/Batman-inicia.jpg/300px-Batman-inicia.jpg'
                 },
-                {title: 'Gran torino',
-                 year: 2015,
-                 img: 'https://2.bp.blogspot.com/_2Syv7WWYErE/S_FRPIb2QyI/AAAAAAAABP0/DBBINTiZCzs/s1600/Clint+Eastwood+-+Gran+Torino+(2).jpg'
+                {   title: 'Gran torino',
+                    year: 2015,
+                     img: 'https://2.bp.blogspot.com/_2Syv7WWYErE/S_FRPIb2QyI/AAAAAAAABP0/DBBINTiZCzs/s1600/Clint+Eastwood+-+Gran+Torino+(2).jpg'
                 },
-                {title: 'Lord of the Rings',
-                 year: 2003,
-                 img: 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg'
+                {   title: 'Lord of the Rings',
+                    year: 2003,
+                    img: 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg'
+                },
+                {
+                    title: 'Dr Strange',
+                    year: 2016,
+                    img: 'https://www.cinemascomics.com/wp-content/uploads/2017/04/7OpmunCEZo93nyRIbx59QRaFvZz.jpg.webp'
                 }
             ]
         }
